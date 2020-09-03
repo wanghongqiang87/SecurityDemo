089e4f61c23cd6f471e2b024c31ac83c18972d29
wanghongqiang <wanghongqiang1012@rayootech.com>
2020-09-03 09:21:18 +0800

    sdfgh

--- src/main/java/com/neo/SpringCloudEurekaApplication.java
+++ src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
 @RestController
 public class SpringCloudEurekaApplication {
 
